758243df0d4a7506aae5f49a305bfebbe582e831
Pablo <amepablo@gmail.com>
2021-12-10 11:29:27 -0300

    Interactuando con Javascript desde el HTML de todas las funciones geometricas

--- figuras.js
+++ figuras.js
@@ -66,3 +66,88 @@ console.group("Círculo");
 
 console.groupEnd();
 
+
+
+// Aquí interactuamos con el HTML
+
+// Funciones calculos cuadrado
+
+function calcularPerimetroCuadrado(){
+
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
+
+}
+
+function calcularAreaCuadrado(){
+
+    const input = document.getElementById("inputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area);
+
+}
+
+// Funciones calculos triángulo
+
+    // Perimetro
+    function calcularPerimetroTriangulo(){
+
+        const inputL1Tri = document.getElementById("inputL1Triangulo");
+        const inputL2Tri = document.getElementById("inputL2Triangulo");
+        const inputbaTri = document.getElementById("inputbaTriangulo");
+        const valueL1Tri = Number(inputL1Tri.value);
+        const valueL2Tri = Number(inputL2Tri.value) ;
+        const valuebaTri = Number(inputbaTri.value);
+
+        const perimetrotria = perimetroTriangulo(valueL1Tri,valueL2Tri,valuebaTri);
+        alert(perimetrotria);
+
+    }
+    // Área
+    function calcularAreaTriangulo() {
+
+        const inputbaTri = document.getElementById("inputbaTriangulo");
+        const inputAlTri = document.getElementById("inputAlTriangulo");
+        const valuebaTri = Number(inputbaTri.value);
+        const valueAlTri = Number(inputAlTri.value);
+        const areaTri = areaTriangulo(valuebaTri,valueAlTri);
+        alert(areaTri);
+
+    }
+
+
+// Funciones calculos círculo
+
+    // Diámetro
+    function calcularDiametroCirculo(){
+
+        const inputRaCirculo = document.getElementById("inputRaCirculo");
+        const valueRaCir = Number(inputRaCirculo.value);
+        const diametroCir = diametroCirculo(valueRaCir);
+        alert(diametroCir);
+
+    }
+
+
+    // Circunferencia
+
+    function calcularCircunferenciaCirculo(){
+
+        const inputRaCirculo = document.getElementById("inputRaCirculo");
+        const valueRaCir = Number(inputRaCirculo.value);
+        const CircuneferenciaCirculo = perimetroCirculo(valueRaCir);
+        alert(CircuneferenciaCirculo);
+    }
+
+    // Área
+
+    function calcularAreaCirculo(){
+        const inputRaCirculo = document.getElementById("inputRaCirculo");
+        const valueRaCir = Number(inputRaCirculo.value);
+        const areaCir = areaCirculo(valueRaCir);
+        alert(areaCir);
+    }
+
