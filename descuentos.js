f660d68bbc6b322a4e29d0cb5b5eb6f441c66d60
Pablo <amepablo@gmail.com>
2021-12-13 12:21:35 -0300

    se agrego la opción de cupones de descuento

--- descuentos.js
+++ descuentos.js
@@ -1,13 +1,50 @@
 // const preciOriginal = 120;
 // const descuento = 18;
 
+
+
 function calcularPreciConDescuento(precio,descuento){
     
-    const porcentajePrecioConDescuento = 100 - descuento;
+    const porcentajePrecioConDescuento = 100 - (descuento);
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
 }
+
+
+
+function onClickButtonPriceDiscount() {
+
+    const array = [0, 10, 15, 20];
+
+    const price = document.getElementById("inputPrice");
+    const priceval = price.value;
+    
+    const discount = document.getElementById("inputDiscount")
+    const discountval = discount.value;
+
+    const cupones = document.getElementById("cupones");
+    const cupon = cupones.value;
+
+    if(cupon == array[0]){
+        cuponDescuento = 0;
+    }else if (cupon == array[1]){
+        cuponDescuento = 10;
+    }else if (cupon == array[2]){
+        cuponDescuento = 15;
+    }else if (cupon == array[3]){
+        cuponDescuento = 20;
+    }
+
+    DescuentoFinal = parseInt(discountval) + parseInt(cuponDescuento);
+
+
+    const precioConDescuento = calcularPreciConDescuento(priceval,DescuentoFinal);
+
+    const resultPrice = document.getElementById("resultP");
+    resultPrice.innerText = "el precio con descuento es: $" + precioConDescuento;
+}
+
 
 // Imprimir varias variables en un console.log usando un objeto
 // console.log({
